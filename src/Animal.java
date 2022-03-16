afd720007bf4defa023d3c447451bdd4a3234a0f
prabhat-deloitte <praparihar@deloitte.com>
2022-03-16 13:30:09 +0530

    "Animal private variable solution"

--- src/Animal.java
+++ src/Animal.java
@@ -1,2 +1,32 @@
 public class Animal {
+    private String Color , Breed , Category;
+    public void setColor( String color){
+         this.Color = color;
+    }public void setBreed(String breed){
+        this.Breed = breed;
+    }public void setCategory(String category){
+        this.Category = category;
+    }
+    public String getColor(){
+        return this.Color;
+    }public String getBreed(){
+        return this.Breed;
+    }public String getCategory(){
+        return this.Category;
+    }
+}
+class Animal_solution{
+    public static void main(String[] args) {
+        //Creating a object of Animal class
+        Animal animal = new Animal();
+        // Setting up the Private variables
+        animal.setColor("brown");
+        animal.setBreed("boxer");
+        animal.setCategory("Dog");
+        // Accessing the Private variables
+        System.out.println(animal.getColor());
+        System.out.println(animal.getBreed());
+        System.out.println(animal.getCategory());
+
+    }
 }
